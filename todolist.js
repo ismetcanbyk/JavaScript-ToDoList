694980701ec85d2b83f6ee0697bc66cca7e38727
ismetcanbyk <ismetcanbyk@gmail.com>
2022-03-20 00:47:34 +0300

    list

--- todolist.js
+++ todolist.js
@@ -1,1 +1,99 @@
-let item 
+// Ekle butonuna tikladigimizda yeni liste ögesi olusturma
+function newElement() {
+    let li = document.createElement('li');
+    let inputValue = document.querySelector('#task').value;
+    let text = document.createTextNode(inputValue);
+
+    li.appendChild(text);
+   
+    if (inputValue === "" || inputValue === " " || inputValue === "  " || inputValue === "    " || inputValue === "     ") {
+        const errorDOM = document.querySelector("#error")
+        const errorFunction = (errMessage) => `
+        <div class="alert alert-danger text-center" role="alert">
+        ${errMessage}
+        <button type="button" class="close " data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        </button>
+        </div>
+        `
+        errorDOM.innerHTML = errorFunction("Listeye boş ekleme yapamazsınız!")
+    }
+    else {
+        document.querySelector("#list").appendChild(li);
+        const successDOM = document.querySelector("#success")
+        const successFunction = (sucMessage) => `
+        <div class="alert alert-success text-center" role="alert">
+        ${sucMessage}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        </button>
+        </div>
+        `
+        successDOM.innerHTML = successFunction ("Listeye eklendi.")
+    }
+    document.querySelector("#task").value="";
+    
+    let span = document.createElement('span');
+    let script = document.createElement('script');
+    script.setAttribute('src', 'https://cdn.lordicon.com/lusqsztk.js')
+    let lordicon = document.createElement('lord-icon')
+    lordicon.setAttribute('src', 'https://cdn.lordicon.com/qsloqzpf.json')
+    lordicon.setAttribute('trigger', 'hover')
+    lordicon.setAttribute('colors', 'primary:#121331')
+    lordicon.setAttribute('style', 'width:24px;height:24px')
+    
+    span.className='close0';
+    span.appendChild(lordicon);
+    li.appendChild(span);
+
+    for (let i=0; i<close.length; i++) {
+        close[i].onclick = function() {
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+
+}
+//Entere basıldığında ekleme
+let input = document.querySelector('#task')
+input.addEventListener("keyup", function(event){
+    if (event.keyCode === 13){
+        event.preventDefault();
+    document.getElementById("liveToastBtn").click();}
+});
+
+
+// Kapatma dugmesi olusturma ve bunu tum liste ogelerine ekleme
+let myList = document.getElementsByTagName('LI');
+var i;
+for (i=0; i<myList.length; i++) {
+    let span = document.createElement('SPAN');
+    let script = document.createElement('SCRIPT');
+    script.setAttribute('src', 'https://cdn.lordicon.com/lusqsztk.js');
+    let lordicon = document.createElement('lord-icon');
+    lordicon.setAttribute('src', 'https://cdn.lordicon.com/qsloqzpf.json');
+    lordicon.setAttribute('trigger', 'hover');
+    lordicon.setAttribute('colors', 'primary:#121331');
+    lordicon.setAttribute('style', 'width:24px;height:24px');
+    span.className="close0";
+    span.appendChild(lordicon);
+    myList[i].appendChild(span);
+}
+
+// Kapat butonuna tiklandiginda gecerli liste ogesini gizlemek
+let close = document.getElementsByClassName('close0');
+var i;
+for (i=0; i < close.length; i++) {
+    close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+
+// Liste ogesine tiklandiginda isaretleme
+let list =document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked')
+    }
+}, false)
